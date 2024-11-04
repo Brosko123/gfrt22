2c3fb700b16f4295fb9062de96a6fa7bc353bc1b
Brosko123 <ytur123@tutamail.com>
2024-11-04 09:10:08 +0530

    Delete server1.js

--- server1.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-"use strict";import s from"express";import o from"./src/params.js";import r from"./src/proxy.js";const t=process.env.PORT||8080;const e=s();e.get("/",o,r);e.get("/favicon.ico",(s,o)=>o.status(204).end());e.listen(t,()=>console.log(`Listening on ${t}`));
