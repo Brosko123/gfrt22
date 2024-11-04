b508dff94cc7d8590aa9ee9b6ed470ea1c6384c9
Brosko123 <ytur123@tutamail.com>
2024-11-04 09:10:18 +0530

    Delete index.js

--- index.js
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env node
-"use strict";import r from"express";import s from"./src/params.js";import t from"./src/proxy.js";const o=r();o.get("/",s,t);o.get("/favicon.ico",(r,s)=>s.status(204).end());export default o;
